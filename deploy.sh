fd5f6780d3be7cbb42bc854088d33a023638d8a6
Frédéric Briol <fbriol@cls.fr>
2018-10-29 15:22:56 +0100

    You must use apache otherwise some requests are invalid.

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ add-apt-repository ppa:webupd8team/java
 apt update
 apt install oracle-java8-installer
 
+IP='192.168.56.102'
+IP_MASK='192.168.56\\.\\d+/'
+
 [ $(getent group tomcat) ] || groupadd tomcat
 [ $(getent passwd tomcat) ] || useradd -g tomcat -s /bin/false -d /opt/tomcat -c "Apache Tomcat daemon" tomcat
 
@@ -13,6 +16,8 @@ wget $URL
 mkdir -p /opt/tomcat
 tar xzvf $(basename $URL) -C /opt/tomcat --strip-components=1
 chown -R tomcat:tomcat /opt/tomcat
+mkdir -p /opt/tomcat/hyrax
+chown -R tomcat:tomcat /opt/tomcat/hyrax
 
 cat <<EOF >/etc/systemd/system/tomcat.service 
 [Unit]
@@ -23,6 +28,7 @@ After=network.target
 Type=forking
 
 Environment='JAVA_HOME=/usr/lib/jvm/java-8-oracle/'
+Environment='OLFS_CONFIG_DIR=/opt/tomcat/hyrax'
 Environment='CATALINA_PID=/opt/tomcat/temp/tomcat.pid'
 Environment='CATALINA_HOME=/opt/tomcat'
 Environment='CATALINA_BASE=/opt/tomcat'
@@ -50,11 +56,8 @@ cat <<EOF >>/opt/tomcat/conf/tomcat-users.xml
 </tomcat-users>
 EOF
 
-systemctl daemon-reload
-systemctl start tomcat
-
-sed -i 's/:1|0:0:0:0:0:0:0:1/:1|0:0:0:0:0:0:0:1|192.168.56\\.\\d+/' /opt/tomcat/webapps/manager/META-INF/context.xml
-sed -i 's/:1|0:0:0:0:0:0:0:1/:1|0:0:0:0:0:0:0:1|192.168.56\\.\\d+/' /opt/tomcat/webapps/host-manager/META-INF/context.xml
+sed -i "s/:1|0:0:0:0:0:0:0:1/:1|0:0:0:0:0:0:0:1|$IP_MASK" /opt/tomcat/webapps/manager/META-INF/context.xml
+sed -i "s/:1|0:0:0:0:0:0:0:1/:1|0:0:0:0:0:0:0:1|$IP_MASK" /opt/tomcat/webapps/host-manager/META-INF/context.xml
 
 keytool -genkey -alias tomcat -keyalg RSA -keystore /opt/tomcat/keystore \
     -dname "cn=Unknown, ou=Unknown, o=Unknown, c=Unknown"  \
@@ -78,16 +81,8 @@ cat <<EOF >/opt/tomcat/conf/server.xml
   See the License for the specific language governing permissions and
   limitations under the License.
 -->
-<!-- Note:  A "Server" is not itself a "Container", so you may not
-     define subcomponents such as "Valves" at this level.
-     Documentation at /docs/config/server.html
- -->
 <Server port="8005" shutdown="SHUTDOWN">
   <Listener className="org.apache.catalina.startup.VersionLoggerListener" />
-  <!-- Security listener. Documentation at /docs/config/listeners.html
-  <Listener className="org.apache.catalina.security.SecurityListener" />
-  -->
-  <!--APR library loader. Documentation at /docs/apr.html -->
   <Listener className="org.apache.catalina.core.AprLifecycleListener" SSLEngine="on" />
   <!-- Prevent memory leaks due to use of particular java/javax APIs-->
   <Listener className="org.apache.catalina.core.JreMemoryLeakPreventionListener" />
@@ -108,54 +103,13 @@ cat <<EOF >/opt/tomcat/conf/server.xml
               pathname="conf/tomcat-users.xml" />
   </GlobalNamingResources>
 
-  <!-- A "Service" is a collection of one or more "Connectors" that share
-       a single "Container" Note:  A "Service" is not itself a "Container",
-       so you may not define subcomponents such as "Valves" at this level.
-       Documentation at /docs/config/service.html
-   -->
   <Service name="Catalina">
-
-    <!--The connectors can use a shared executor, you can define one or more named thread pools-->
-    <!--
     <Executor name="tomcatThreadPool" namePrefix="catalina-exec-"
-        maxThreads="150" minSpareThreads="4"/>
-    -->
+        maxThreads="200" minSpareThreads="4"/>
 
-
-    <!-- A "Connector" represents an endpoint by which requests are received
-         and responses are returned. Documentation at :
-         Java HTTP Connector: /docs/config/http.html
-         Java AJP  Connector: /docs/config/ajp.html
-         APR (HTTP/AJP) Connector: /docs/apr.html
-         Define a non-SSL/TLS HTTP/1.1 Connector on port 8080
-    -->
     <Connector port="8080" protocol="HTTP/1.1"
                connectionTimeout="20000"
                redirectPort="8443" />
-    <!-- A "Connector" using the shared thread pool-->
-    <!--
-    <Connector executor="tomcatThreadPool"
-               port="8080" protocol="HTTP/1.1"
-               connectionTimeout="20000"
-               redirectPort="8443" />
-    -->
-    <!-- Define a SSL/TLS HTTP/1.1 Connector on port 8443
-         This connector uses the NIO implementation. The default
-         SSLImplementation will depend on the presence of the APR/native
-         library and the useOpenSSL attribute of the
-         AprLifecycleListener.
-         Either JSSE or OpenSSL style configuration may be used regardless of
-         the SSLImplementation selected. JSSE style configuration is used below.
-    -->
-    <!--
-    <Connector port="8443" protocol="org.apache.coyote.http11.Http11NioProtocol"
-               maxThreads="150" SSLEnabled="true">
-        <SSLHostConfig>
-            <Certificate certificateKeystoreFile="conf/localhost-rsa.jks"
-                         type="RSA" />
-        </SSLHostConfig>
-    </Connector>
-    -->
 
     <Connector protocol="org.apache.coyote.http11.Http11NioProtocol"
             port="8443" maxThreads="200"
@@ -164,70 +118,17 @@ cat <<EOF >/opt/tomcat/conf/server.xml
             KeystorePass="s3cret"
             clientAuth="false" sslProtocol="TLS" />
 
-    <!-- Define a SSL/TLS HTTP/1.1 Connector on port 8443 with HTTP/2
-         This connector uses the APR/native implementation which always uses
-         OpenSSL for TLS.
-         Either JSSE or OpenSSL style configuration may be used. OpenSSL style
-         configuration is used below.
-    -->
-    <!--
-    <Connector port="8443" protocol="org.apache.coyote.http11.Http11AprProtocol"
-               maxThreads="150" SSLEnabled="true" >
-        <UpgradeProtocol className="org.apache.coyote.http2.Http2Protocol" />
-        <SSLHostConfig>
-            <Certificate certificateKeyFile="conf/localhost-rsa-key.pem"
-                         certificateFile="conf/localhost-rsa-cert.pem"
-                         certificateChainFile="conf/localhost-rsa-chain.pem"
-                         type="RSA" />
-        </SSLHostConfig>
-    </Connector>
-    -->
-
     <!-- Define an AJP 1.3 Connector on port 8009 -->
     <Connector port="8009" protocol="AJP/1.3" redirectPort="8443" />
 
-
-    <!-- An Engine represents the entry point (within Catalina) that processes
-         every request.  The Engine implementation for Tomcat stand alone
-         analyzes the HTTP headers included with the request, and passes them
-         on to the appropriate Host (virtual host).
-         Documentation at /docs/config/engine.html -->
-
-    <!-- You should set jvmRoute to support load-balancing via AJP ie :
-    <Engine name="Catalina" defaultHost="localhost" jvmRoute="jvm1">
-    -->
     <Engine name="Catalina" defaultHost="localhost">
-
-      <!--For clustering, please take a look at documentation at:
-          /docs/cluster-howto.html  (simple how to)
-          /docs/config/cluster.html (reference documentation) -->
-      <!--
-      <Cluster className="org.apache.catalina.ha.tcp.SimpleTcpCluster"/>
-      -->
-
-      <!-- Use the LockOutRealm to prevent attempts to guess user passwords
-           via a brute-force attack -->
       <Realm className="org.apache.catalina.realm.LockOutRealm">
-        <!-- This Realm uses the UserDatabase configured in the global JNDI
-             resources under the key "UserDatabase".  Any edits
-             that are performed against this UserDatabase are immediately
-             available for use by the Realm.  -->
         <Realm className="org.apache.catalina.realm.UserDatabaseRealm"
                resourceName="UserDatabase"/>
       </Realm>
 
       <Host name="localhost"  appBase="webapps"
             unpackWARs="true" autoDeploy="true">
-
-        <!-- SingleSignOn valve, share authentication between web applications
-             Documentation at: /docs/config/valve.html -->
-        <!--
-        <Valve className="org.apache.catalina.authenticator.SingleSignOn" />
-        -->
-
-        <!-- Access log processes all example.
-             Documentation at: /docs/config/valve.html
-             Note: The pattern used is equivalent to using pattern="common" -->
         <Valve className="org.apache.catalina.valves.AccessLogValve" directory="logs"
                prefix="localhost_access_log" suffix=".txt"
                pattern="%h %l %u %t &quot;%r&quot; %s %b" />
@@ -263,3 +164,188 @@ cat <<EOF >>/opt/tomcat/conf/tomcat-users.xml
 </tomcat-users>
 EOF
 
+cat <<EOF >/opt/tomcat/.ncWMS2/config.xml
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<config>
+    <datasets>
+        <!--dataset id="coads_climatology" title="COADS via DAP2" location="http://localhost:8080/opendap/data/nc/coads_climatology.nc" queryable="true" downloadable="false" dataReaderClass="" copyrightStatement="" moreInfo="" disabled="false" updateInterval="-1">
+            <variables/>
+        </dataset -->
+    </datasets>
+    <cache enabled="true">
+        <inMemorySizeMB>256</inMemorySizeMB>
+        <elementLifetimeMinutes>10.0</elementLifetimeMinutes>
+    </cache>
+    <dynamicServices>
+        <dynamicService alias="lds" servicePath="http://localhost/opendap/" datasetIdMatch=".*" dataReaderClass="" copyrightStatement="" moreInfoUrl="" disabled="false" queryable="true" downloadable="false"/>
+    </dynamicServices>
+    <contact>
+        <name>OPeNDAP Docker</name>
+        <organization>OPeNDAP</organization>
+        <telephone></telephone>
+        <email>support@opendap.og</email>
+    </contact>
+    <server>
+        <title>ncWMS Server</title>
+        <allowFeatureInfo>true</allowFeatureInfo>
+        <maxImageWidth>1024</maxImageWidth>
+        <maxImageHeight>1024</maxImageHeight>
+        <abstract></abstract>
+        <keywords></keywords>
+        <url>http://$IP/ncWMS2</url>
+        <allowglobalcapabilities>true</allowglobalcapabilities>
+    </server>
+</config>
+EOF
+chown tomcat:tomcat /opt/tomcat/.ncWMS2/config.xml
+
+service tomcat start
+service tomcat stop
+
+cat <<EOF >/opt/tomcat/hyrax/viewers.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+  ~ /////////////////////////////////////////////////////////////////////////////
+  ~ // This file is part of the "Hyrax Data Server" project.
+  ~ //
+  ~ //
+  ~ // Copyright (c) 2013 OPeNDAP, Inc.
+  ~ // Author: Nathan David Potter  <ndp@opendap.org>
+  ~ //
+  ~ // This library is free software; you can redistribute it and/or
+  ~ // modify it under the terms of the GNU Lesser General Public
+  ~ // License as published by the Free Software Foundation; either
+  ~ // version 2.1 of the License, or (at your option) any later version.
+  ~ //
+  ~ // This library is distributed in the hope that it will be useful,
+  ~ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ~ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  ~ // Lesser General Public License for more details.
+  ~ //
+  ~ // You should have received a copy of the GNU Lesser General Public
+  ~ // License along with this library; if not, write to the Free Software
+  ~ // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+  ~ //
+  ~ // You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+  ~ /////////////////////////////////////////////////////////////////////////////
+  -->
+
+<ViewersConfig>
+
+    <JwsHandler className="opendap.webstart.IdvViewerRequestHandler">
+        <JnlpFileName>idv.jnlp</JnlpFileName>
+    </JwsHandler>
+
+    <JwsHandler className="opendap.webstart.NetCdfToolsViewerRequestHandler">
+        <JnlpFileName>idv.jnlp</JnlpFileName>
+    </JwsHandler>
+
+    <JwsHandler className="opendap.webstart.AutoplotRequestHandler" />
+
+
+    <!--
+     If you are using Hyrax with our ncWMS release you'll need to configure both
+     Hyrax (in the section below) and ncWMS to make it all work. Detailed instructions
+     for how to do this can be found at: http://docs.opendap.org/index.php/Hyrax_WMS
+
+     Note: Un-commenting the two WebServiceHandler definitions below will cause Hyrax
+     to add links to the WMS service and the Godiva web client to each dataset's "viewers"
+     page and to the list of default services used in both static and dynamic THREDDS
+     catalogs returned by the server.
+     -->
+
+    <WebServiceHandler className="opendap.viewers.NcWmsService" serviceId="ncWms" >
+        <applicationName>Web Mapping Service</applicationName>
+        <NcWmsService href="http://$IP/ncWMS2/wms" base="http://$IP/ncWMS2/wms" ncWmsDynamicServiceId="lds" />
+    </WebServiceHandler>
+
+    <WebServiceHandler className="opendap.viewers.GodivaWebService" serviceId="godiva" >
+        <applicationName>Godiva WMS GUI</applicationName>
+        <NcWmsService href="http://$IP:8080/ncWMS2/wms" base="/ncWMS2/wms" ncWmsDynamicServiceId="lds"/>
+        <Godiva href="http://$IP/ncWMS2/Godiva3.html" base="http://$IP/ncWMS2/Godiva3.html"/>
+    </WebServiceHandler>
+
+    <!--
+    <WebServiceHandler className="opendap.viewers.WcsService" serviceId="WCS-COADS" >
+        <ApplicationName>COADS Climatology WCS Service/</ApplicationName>
+        <ServiceEndpoint>http://$IP/opendap/wcs/</ServiceEndpoint>
+        <MatchRegex>^.*coads.*\.nc$</MatchRegex>
+        <DynamicServiceId>coads</DynamicServiceId>
+    </WebServiceHandler>
+    -->
+</ViewersConfig>
+EOF
+
+apt install apache2
+mv /etc/apache2/mods-available/proxy.load /etc/apache2/mods-enabled
+mv /etc/apache2/mods-available/proxy_ajp.load /etc/apache2/mods-enabled
+
+cat <<EOF >/etc/apache2/sites-enabled/000-default.conf 
+<VirtualHost *:80>
+        # The ServerName directive sets the request scheme, hostname and port that
+        # the server uses to identify itself. This is used when creating
+        # redirection URLs. In the context of virtual hosts, the ServerName
+        # specifies what hostname must appear in the request's Host: header to
+        # match this virtual host. For the default virtual host (this file) this
+        # value is not decisive as it is used as a last resort host regardless.
+        # However, you must set it for any further virtual host explicitly.
+        #ServerName www.example.com
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+
+        <Proxy *>
+          AddDefaultCharset Off
+          Order deny,allow
+          Allow from all
+        </Proxy>
+
+        ProxyPass /opendap ajp://localhost:8009/opendap
+        ProxyPass /ncWMS2 ajp://localhost:8009/ncWMS2
+        ProxyPassReverse /opendap ajp://localhost:8009/opendap
+        
+        <Location />
+          # Insert filter
+          SetOutputFilter DEFLATE
+
+          # Netscape 4.x has some problems...
+          BrowserMatch ^Mozilla/4 gzip-only-text/html
+
+          # Netscape 4.06-4.08 have some more problems
+          BrowserMatch ^Mozilla/4\.0[678] no-gzip
+
+          # MSIE masquerades as Netscape, but it is fine
+          # BrowserMatch \bMSIE !no-gzip !gzip-only-text/html
+
+          # NOTE: Due to a bug in mod_setenvif up to Apache 2.0.48
+          # the above regex won't work. You can use the following
+          # workaround to get the desired effect:
+          BrowserMatch \bMSI[E] !no-gzip !gzip-only-text/html
+
+          # Don't compress images
+          SetEnvIfNoCase Request_URI \
+          \.(?:gif|jpe?g|png)$ no-gzip dont-vary
+
+          # Make sure proxies don't deliver the wrong content
+          # Header append Vary User-Agent env=!dont-vary
+        </Location>
+
+        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+        # error, crit, alert, emerg.
+        # It is also possible to configure the loglevel for particular
+        # modules, e.g.
+        #LogLevel info ssl:warn
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        # For most configuration files from conf-available/, which are
+        # enabled or disabled at a global level, it is possible to
+        # include a line for only one particular virtual host. For example the
+        # following line enables the CGI configuration for this host only
+        # after it has been globally disabled with "a2disconf".
+        #Include conf-available/serve-cgi-bin.conf
+</VirtualHost>
+
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
+EOF
